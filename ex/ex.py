f2a8afd721fc062f5dcb876bd0e31c2d0e176459
juhong bae <baejoohong93@gmail.com>
2025-05-19 17:16:09 +0900

    250519 html 수업완료

--- ex/ex.py
+++ ex/ex.py
@@ -1,10 +1,43 @@
-# # y,m,d = input().split(':')
-# # print(d,m,y, sep=':')
+# # # y,m,d = input().split(':')
+# # # print(d,m,y, sep=':')
 
-a,b = input("주민번호 입력하시오: ").split(':')
-print(a,b, sep='-')
+# a,b = input("주민번호 입력하시오: ").split(':')
+# print(a,b, sep='-')
 
 
 
-# jumin = input()
-# print(*jumin.split('-'))
+# # jumin = input()
+# # print(*jumin.split('-'))
+
+'''
+num % 2 == 0
+even_numbers.apppend(num)
+
+
+a = float(input("온도 입력: "))
+if a >= 100:
+    print("수증기")
+elif a >= 0:
+    print("물")
+else:
+    print("얼음")
+
+
+a = sum(scores.values())
+b = a / len(scores)
+print(f"평균: {b}")
+
+
+a = "Python Programming"
+b = a.replace(" ","")
+c = b.lower()
+print(c)
+
+
+a = int(input("숫자 입력: "))
+b = 0
+for i in range(1,a+1):
+    if i % 3 != 0:
+        b += i
+print(b)
+'''
